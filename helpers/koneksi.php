b07167da8973b39a2d3ba1d2dd29687a88bc24a6
VYNG85 <71924265+vynkevin222@users.noreply.github.com>
2021-10-15 22:04:32 +0700

    Add files via upload

---
+++ helpers/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "mysql-54502-0.cloudclusters.net";
+$username = "admin";
+$password = "RNZgBemA";
+$db = "wa_sysdev";
+
+$koneksi = mysqli_connect($host, $username, $password, $db) or die("GAGAL");
+
+$base_url = "http://localhost/wa-sysdev/";
+date_default_timezone_set('Asia/Jakarta');
+?>
